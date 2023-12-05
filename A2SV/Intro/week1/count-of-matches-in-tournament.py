6766c40a6ac545388ad06b63e6d5c9a2e134c6a4
Fanual Asfaw <106735395+fanual-software-dev@users.noreply.github.com>
2023-12-05 17:13:48 +0300

    Added solution for Count of Matches in Tournament

---
+++ A2SV/Intro/week1/count-of-matches-in-tournament.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def numberOfMatches(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        ans=0
+
+        while n>1:
+
+            if n%2==0:
+                ans+=(n/2)
+                n=n/2
+            else:
+                ans+=((n-1)/2)
+                n=(n-1)/2+1
+        return ans
