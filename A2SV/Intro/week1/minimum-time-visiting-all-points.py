18c80f81476302df14ffabac883ff746228b22af
Fanual Asfaw <106735395+fanual-software-dev@users.noreply.github.com>
2023-12-05 22:24:42 +0300

    Added solution for Minimum Time Visiting All Points

---
+++ A2SV/Intro/week1/minimum-time-visiting-all-points.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def minTimeToVisitAllPoints(self, points):
+        """
+        :type points: List[List[int]]
+        :rtype: int
+        """
+
+        total=0
+
+        for i in range(1,len(points)):
+            total+=max(abs(points[i][0]-points[i-1][0]),abs(points[i][1]-points[i-1][1]))
+        return total
