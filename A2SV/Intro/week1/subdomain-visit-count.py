3f0b1de64ae0b024eb4fec635d949857d7cce7c1
Fanual Asfaw <106735395+fanual-software-dev@users.noreply.github.com>
2023-12-09 00:05:10 +0300

    Added solution for Subdomain Visit Count

---
+++ A2SV/Intro/week1/subdomain-visit-count.py
@@ -0,0 +1,23 @@
+class Solution:
+    def subdomainVisits(self, cpdomains: List[str]) -> List[str]:
+
+        ans = []
+
+        ans_dict = {}
+
+        for domains in cpdomains:
+
+            value, other = domains.split()
+
+            ans = other.split(".")
+
+            print(ans)
+
+            for i in range(len(ans)):
+
+                ans_dict [".".join(ans[i:])]= int(value) + ans_dict.get(".".join(ans[i:]),0)
+
+        rans = [f'{value} {key}' for key ,value in ans_dict.items()]
+
+        return rans 
+        
