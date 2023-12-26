195f1fbdd323de4b589d0ea4058ae5b62b2aeca2
Fanual Asfaw <106735395+fanual-software-dev@users.noreply.github.com>
2023-12-26 19:15:18 +0300

    Added solution for Sort the People

--- A2SV/Intro/week1/sort-the-people.py
+++ A2SV/Intro/week1/sort-the-people.py
@@ -2,14 +2,22 @@ class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
 
         
-        for i in range(len(heights)):
+        for i in range(1,len(heights)):
+            
+            right = i
 
-            for j in range(len(heights)):
+            while right>0 and heights[right-1]<heights[right]:
 
-                if heights[i]>heights[j]:
-
-                    heights[i],heights[j] =  heights[j],heights[i] 
-                    names[i],names[j] = names[j], names[i]
+                heights[right-1],heights[right] = heights[right],heights[right-1]
+                names[right-1], names[right] =  names[right], names[right-1]
+                right-=1
+        
         return names
+        
+        
+
+
+
+            
         
         
